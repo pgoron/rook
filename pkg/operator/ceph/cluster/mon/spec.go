45894802ceb3810cf4470fae548584b5febb7139
Peter Goron <p.goron@criteo.com>
2022-07-14 00:03:42 +0200

    Use mon tcp-msgr2 port value from cluster spec

--- pkg/operator/ceph/cluster/mon/spec.go
+++ pkg/operator/ceph/cluster/mon/spec.go
@@ -338,7 +338,7 @@ func (c *Cluster) makeMonDaemonContainer(monConfig *monConfig) corev1.Container 
 	}
 
 	// Add messenger 2 port
-	addContainerPort(container, "tcp-msgr2", 3300)
+	addContainerPort(container, "tcp-msgr2", c.spec.Mon.Msgr2Port)
 
 	return container
 }

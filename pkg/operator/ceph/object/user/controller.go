pgoron/rook

commit f91e794c4dc3f6ecbd2a0ebb2dc5f5c045fc8179

fix missing admin capabilities for cephobjectstoreusers
users and buckets admin capabilities are supported by ceph (see
https://github.com/ceph/ceph/blob/main/src/rgw/rgw_common.cc#L2093)
but are discarded by rook/kubernetes because they aren't part
of the CRD.

pkg/operator/ceph/object/user/controller.go
@@ -407,8 +407,11 @@ func generateUserConfig(user *cephv1.CephObjectStoreUser) admin.User {
 		if user.Spec.Capabilities.User != "" {
 			userConfig.UserCaps += fmt.Sprintf("users=%s;", user.Spec.Capabilities.User)
 		}
-		if user.Spec.Capabilities.Bucket != "" {
-			userConfig.UserCaps += fmt.Sprintf("buckets=%s;", user.Spec.Capabilities.Bucket)
+		if user.Spec.Capabilities.Users != "" {
+			userConfig.UserCaps += fmt.Sprintf("users=%s;", user.Spec.Capabilities.User)
+		}
+		if user.Spec.Capabilities.Buckets != "" {
+			userConfig.UserCaps += fmt.Sprintf("buckets=%s;", user.Spec.Capabilities.Buckets)
 		}
 		if user.Spec.Capabilities.MetaData != "" {
 			userConfig.UserCaps += fmt.Sprintf("metadata=%s;", user.Spec.Capabilities.MetaData)

pkg/operator/ceph/object/user/controller_test.go
@@ -448,8 +448,8 @@ func TestCreateOrUpdateCephUser(t *testing.T) {
 	t.Run("setting Capabilities for the user", func(t *testing.T) {
 		objectUser.Spec.Quotas = nil
 		objectUser.Spec.Capabilities = &cephv1.ObjectUserCapSpec{
-			User:   "read",
-			Bucket: "read",
+			User:    "read",
+			Buckets: "read",
 		}
 		userConfig = generateUserConfig(objectUser)
 		r.userConfig = &userConfig
@@ -497,8 +497,8 @@ func TestCreateOrUpdateCephUser(t *testing.T) {
 
 	t.Run("setting both Quotas and Capabilities for the user", func(t *testing.T) {
 		objectUser.Spec.Capabilities = &cephv1.ObjectUserCapSpec{
-			User:   "read",
-			Bucket: "read",
+			User:    "read",
+			Buckets: "read",
 		}
 		objectUser.Spec.Quotas = &cephv1.ObjectUserQuotaSpec{MaxBuckets: &maxbucket, MaxObjects: &maxobject, MaxSize: &maxsize}
 		userConfig = generateUserConfig(objectUser)

deploy/charts/rook-ceph/templates/resources.yaml
@@ -8892,7 +8892,7 @@ spec:
                   description: Additional admin-level capabilities for the Ceph object store user
                   nullable: true
                   properties:
-                    bucket:
+                    buckets:
                       description: Admin capabilities to read/write Ceph object store buckets. Documented in https://docs.ceph.com/en/latest/radosgw/admin/?#add-remove-admin-capabilities
                       enum:
                         - '*'
@@ -8917,6 +8917,14 @@ spec:
                         - read, write
                       type: string
                     user:
+                      description: Admin capabilities to read/write Ceph object store users. Documented in https://docs.ceph.com/en/latest/radosgw/admin/?#add-remove-admin-capabilities
+                      enum:
+                        - '*'
+                        - read
+                        - write
+                        - read, write
+                      type: string
+                    users:
                       description: Admin capabilities to read/write Ceph object store users. Documented in https://docs.ceph.com/en/latest/radosgw/admin/?#add-remove-admin-capabilities
                       enum:
                         - '*'

deploy/examples/crds.yaml
@@ -8883,7 +8883,7 @@ spec:
                   description: Additional admin-level capabilities for the Ceph object store user
                   nullable: true
                   properties:
-                    bucket:
+                    buckets:
                       description: Admin capabilities to read/write Ceph object store buckets. Documented in https://docs.ceph.com/en/latest/radosgw/admin/?#add-remove-admin-capabilities
                       enum:
                         - '*'
@@ -8908,6 +8908,14 @@ spec:
                         - read, write
                       type: string
                     user:
+                      description: Admin capabilities to read/write Ceph object store users. Documented in https://docs.ceph.com/en/latest/radosgw/admin/?#add-remove-admin-capabilities
+                      enum:
+                        - '*'
+                        - read
+                        - write
+                        - read, write
+                      type: string
+                    users:
                       description: Admin capabilities to read/write Ceph object store users. Documented in https://docs.ceph.com/en/latest/radosgw/admin/?#add-remove-admin-capabilities
                       enum:
                         - '*'

pkg/apis/ceph.rook.io/v1/types.go
@@ -1519,8 +1519,12 @@ type ObjectUserCapSpec struct {
 	User string `json:"user,omitempty"`
 	// +optional
 	// +kubebuilder:validation:Enum={"*","read","write","read, write"}
+	// Admin capabilities to read/write Ceph object store users. Documented in https://docs.ceph.com/en/latest/radosgw/admin/?#add-remove-admin-capabilities
+	Users string `json:"users,omitempty"`
+	// +optional
+	// +kubebuilder:validation:Enum={"*","read","write","read, write"}
 	// Admin capabilities to read/write Ceph object store buckets. Documented in https://docs.ceph.com/en/latest/radosgw/admin/?#add-remove-admin-capabilities
-	Bucket string `json:"bucket,omitempty"`
+	Buckets string `json:"buckets,omitempty"`
 	// +optional
 	// +kubebuilder:validation:Enum={"*","read","write","read, write"}
 	// Admin capabilities to read/write Ceph object store metadata. Documented in https://docs.ceph.com/en/latest/radosgw/admin/?#add-remove-admin-capabilities

commit 45ff79a6e721f43f7aaffc83193c72b385d2a984

user: fix typo preventing creation of users caps
Issue introduced by https://github.com/criteo-forks/rook/commit/f91e794c4dc3f6ecbd2a0ebb2dc5f5c045fc8179

But fixed before submission to upstream
https://github.com/rook/rook/pull/12460/files

pkg/operator/ceph/object/user/controller.go
@@ -423,7 +423,7 @@ func generateUserConfig(user *cephv1.CephObjectStoreUser) admin.User {
 			userConfig.UserCaps += fmt.Sprintf("users=%s;", user.Spec.Capabilities.User)
 		}
 		if user.Spec.Capabilities.Users != "" {
-			userConfig.UserCaps += fmt.Sprintf("users=%s;", user.Spec.Capabilities.User)
+			userConfig.UserCaps += fmt.Sprintf("users=%s;", user.Spec.Capabilities.Users)
 		}
 		if user.Spec.Capabilities.Buckets != "" {
 			userConfig.UserCaps += fmt.Sprintf("buckets=%s;", user.Spec.Capabilities.Buckets)

pkg/operator/ceph/object/user/controller_test.go
@@ -461,7 +461,7 @@ func TestCreateOrUpdateCephUser(t *testing.T) {
 	t.Run("setting Capabilities for the user", func(t *testing.T) {
 		objectUser.Spec.Quotas = nil
 		objectUser.Spec.Capabilities = &cephv1.ObjectUserCapSpec{
-			User:    "read",
+			Users:   "read",
 			Buckets: "read",
 		}
 		userConfig = generateUserConfig(objectUser)
@@ -510,7 +510,7 @@ func TestCreateOrUpdateCephUser(t *testing.T) {
 
 	t.Run("setting both Quotas and Capabilities for the user", func(t *testing.T) {
 		objectUser.Spec.Capabilities = &cephv1.ObjectUserCapSpec{
-			User:    "read",
+			Users:   "read",
 			Buckets: "read",
 		}
 		objectUser.Spec.Quotas = &cephv1.ObjectUserQuotaSpec{MaxBuckets: &maxbucket, MaxObjects: &maxobject, MaxSize: &maxsize}